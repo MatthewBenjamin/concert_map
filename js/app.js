3157fdf5a1395d281c53fc05161c61ae835013ed
Matt Benjamin <matt.g.benjamin@gmail.com>
2015-05-04 20:46:06 +0900

    Add Google geolocation API to determine center of Google Map based on an address

--- js/app.js
+++ js/app.js
@@ -1,30 +1,41 @@
 // Custom Handler for Google Map
 ko.bindingHandlers.googlemap = {
-    init: function (element, valueAccessor) {
-        var value = valueAccessor();
+    map: null,
+    createMap: function(element, latitude, longitude) {
         var mapOptions = {
             zoom: 12,
-            center: new google.maps.LatLng(value.mapCenter().centerLat, value.mapCenter().centerLon)
+            center: new google.maps.LatLng(latitude, longitude)
         };
-        var map = new google.maps.Map(element, mapOptions);
+        ko.bindingHandlers.googlemap.map = new google.maps.Map(element, mapOptions);
+        console.log(ko.bindingHandlers.googlemap.map);
+    },
+    init: function (element, valueAccessor) {
+        var value = valueAccessor();
 
-        for (var l in value.mapMarkers())
-        {
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode( { 'address': value.currentAddress }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                    var latitude = results[0]['geometry']['location']['A'];
+                    var longitude = results[0]['geometry']['location']['F'];
+                    ko.bindingHandlers.googlemap.createMap(element, latitude, longitude);
+            } else {
+                alert('Geocoder error because: ' + status);
+            }
+        });
+
+        for (var i = 0; i < value.mapMarkers().length; i++){
+            //console.log(value.mapMarkers()[i]);
             var latLng = new google.maps.LatLng(
-                            value.mapMarkers()[l].latitude,
-                            value.mapMarkers()[l].longitude);
+                            value.mapMarkers()[i].latitude,
+                            value.mapMarkers()[i].longitude);
+            //console.log(latLng);
             var marker = new google.maps.Marker({
                 position: latLng,
-                map: map
-              });
+                map: ko.bindingHandlers.googlemap.map
+            });
         }
     }
-};
-
-// Map center on page load
-var mapCenter = {
-    centerLat: 37.5667,
-    centerLon: 126.9667
 };
 
 var mapMarkers = [
@@ -32,35 +43,57 @@ var mapMarkers = [
     {name: "Gangnam", latitude: 37.4967, longitude: 127.0275}
 ];
 
-// Get Meetup API data based on mapCenter
-var getMeetups = function(latitude, longitude) {
-    console.log(latitude, longitude);
-    var request = 'https://api.meetup.com/2/open_events?' +
-    'and_text=False&' +
-    //'offset=0&' +
-    'format=json&' +
-    'lon=' + longitude + '&' +
-    'limited_events=True&' +    // SHOULD THIS BE TRUE OR FALSE? (or don't include?)
-    'photo-host=public&' +      // WHAT DOES THIS DO?
-    'page=20&' +                // TODO: fine tune this value
-    'radius=25.0&' +            // TODO: fine tune this value
-    'lat=' + latitude + '&' +
-    'desc=False&' +             // WHAT DOES THIS DO?
-    'status=upcoming&' +        // DO I NEED THIS?
-    'sig_id=2220302&' +
-    'sig=cd6dba060220731727d2b23f0ae2e28283fa0a69';     // SIG (and potentialy sig_id) will need to be changed
-                                                        // if certain request parameters are changed
-
-    //console.log(request);
-    console.log($.ajax(request));
-};
-
+var centerTest;
 var ViewModel =  function () {
     var self = this;
 
-    self.mapCenter = ko.observable(mapCenter);          // TODO: base this on input from search bar or default value
     self.mapMarkers = ko.observableArray(mapMarkers);
-    self.meetups = ko.observable(getMeetups(self.mapCenter().centerLat, self.mapCenter().centerLon));
+
+    var defaultLocation = 'Seoul, South Korea';
+    self.currentAddress = ko.computed(function(address) {
+        address = address || defaultLocation;
+        return address;
+    });
+
+/*
+    self.init = function() {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { 'address': self.defaultLocation }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                console.log('LAT: ' + results[0]['geometry']['location']['A']);
+                console.log('LNG: ' + results[0]['geometry']['location']['F']);
+            } else {
+                alert('Geocoder error because: ' + status);
+            }
+        });
+    }
+*/
+
+    self.getLastFmEvents = function(latitude, longitude) {
+        var result;
+        var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&' +
+        'lat=' + latitude + '&' +
+        'long=' + longitude + '&' +
+        'limit=10&' +               // TODO: fine tune OR make editable or self correcting
+        'api_key=d824cbbb7759624aa8b3621a627b70b8' +
+        '&format=json'
+
+        var requestSettings = {
+            success: function(data, status, jqXHR) {
+                /*
+                // TODO: pass data to function to create markers/list items
+                var lastFmEvents = data.events.event
+                //console.log(data);
+                for (var i = 0; i < lastFmEvents.length; i++) {
+                    //console.log(lastFmEvents[i]);
+                }
+                */
+            }
+        }
+        $.ajax(requestURL,requestSettings);
+    };
+
+    //self.getLastFmEvents(self.mapCenter().centerLat, self.mapCenter().centerLon);
 };
 
 ko.applyBindings(ViewModel);
