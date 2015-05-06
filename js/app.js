24b20b0267b1987ba945e9966c0daf47b060e36f
Matt Benjamin <matt.g.benjamin@gmail.com>
2015-05-06 17:21:24 +0900

    Refactor google map initialization

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 // Custom Handler for Google Map
 ko.bindingHandlers.googlemap = {
     map: null,
-    createMap: function(element, latitude, longitude) {
-        var mapOptions = {
-            zoom: 13,
-            center: new google.maps.LatLng(latitude, longitude)
-        };
-        ko.bindingHandlers.googlemap.map = new google.maps.Map(element, mapOptions);
-    },
     createMarkers: function(mapMarkers) {
         console.log(mapMarkers);
         var infoWindow = new google.maps.InfoWindow();
@@ -31,70 +24,64 @@ ko.bindingHandlers.googlemap = {
             });
         }
     },
+    init: function (element) {
+        var mapOptions = {
+            zoom: 13,
+        };
+        ko.bindingHandlers.googlemap.map = new google.maps.Map(element, mapOptions);
+    },
     update: function (element, valueAccessor) {
         var value = valueAccessor();
-        //console.log(value.mapCenter);
         var latitude = value.mapCenter.latitude;
         var longitude = value.mapCenter.longitude;
-        ko.bindingHandlers.googlemap.createMap(element, latitude, longitude);
-        //console.log(value.lastFmEvents);
+        ko.bindingHandlers.googlemap.map.setCenter( { lat: latitude, lng: longitude } );
         ko.bindingHandlers.googlemap.createMarkers(value.lastFmEvents);
-        //console.log(value.lastFmEvents)
-        //ko.bindingHandlers.googlemap.getLastFmEvents(latitude, longitude);
-        /*
-        var geocoder = new google.maps.Geocoder();
-
-        geocoder.geocode( { 'address': value.currentAddress }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                    var latitude = results[0]['geometry']['location']['A'];
-                    var longitude = results[0]['geometry']['location']['F'];
-                    ko.bindingHandlers.googlemap.createMap(element, latitude, longitude);
-                    ko.bindingHandlers.googlemap.getLastFmEvents(latitude, longitude);
-                    //ko.bindingHandlers.googlemap.createMarkers(value.mapMarkers());
-                    //mapCenter.latitude = latitude;
-                    //mapCenter.longitude = longitude;
-            } else {
-                alert('Geocoder error because: ' + status);
-            }
-        });
-*/
     }
 };
 
+//var googleMap = ko.bindingHandlers.googlemap;
+
 var ViewModel =  function () {
     var self = this;
-
-    var defaultLocation = 'Austin, TX';
-    var defaultLatLong = {
-        latitude: 30.267153,
-        longitude: -97.74306079999997
-    };
-    self.currentAddress = ko.computed(function(address) {
-        address = address || defaultLocation;
-        return address;
-    });
-    self.mapCenter = ko.computed(function(latlong) {
-        latlong = latlong || defaultLatLong;
-        return latlong;
-    });
+    self.defaultLocation = 'Austin, TX';
+    self.currentAddress = ko.observable(self.defaultLocation);
+    self.mapCenter = ko.observable( { latitude: null, longitude: null } );
     self.lastFmEvents = ko.observableArray();
 
-    self.getLastFmEvents = ko.computed(function() {
-        var latitude = self.mapCenter().latitude;
-        var longitude = self.mapCenter().longitude;
-        var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&' +
-            'lat=' + latitude + '&' +
-            'long=' + longitude + '&' +
-            'limit=10&' +               // TODO: fine tune OR make editable or self correcting
-            'api_key=d824cbbb7759624aa8b3621a627b70b8' +
-            '&format=json'
-        var requestSettings = {
-            success: function(data, status, jqXHR) {
-                self.lastFmEvents(data.events.event);
-                //console.log(self.lastFmEvents());
+    var geocoder = new google.maps.Geocoder();
+    self.getMapGeocode = ko.computed(function() {
+        geocoder.geocode( { 'address': self.currentAddress() }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                    var latitude = results[0]['geometry']['location']['j'];
+                    var longitude = results[0]['geometry']['location']['C'];
+                    var mapCenter = {
+                        latitude: latitude,
+                        longitude: longitude
+                    };
+                    self.mapCenter(mapCenter);
+            } else {
+                alert('Geocoder error because: ' + status);
             }
+        })
+    });
+
+    self.getLastFmEvents = ko.computed(function() {
+        if (self.mapCenter().latitude && self.mapCenter().longitude) {
+            var latitude = self.mapCenter().latitude;
+            var longitude = self.mapCenter().longitude;
+            var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&' +
+                'lat=' + latitude + '&' +
+                'long=' + longitude + '&' +
+                'limit=10&' +               // TODO: fine tune OR make editable or self correcting
+                'api_key=d824cbbb7759624aa8b3621a627b70b8' +
+                '&format=json'
+            var requestSettings = {
+                success: function(data, status, jqXHR) {
+                    self.lastFmEvents(data.events.event);
+                }
+            };
+            $.ajax(requestURL,requestSettings)
         }
-        $.ajax(requestURL,requestSettings)
     });
 };
 
