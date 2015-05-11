1ffe51fd36a7843f1eca7d49a40bd68577e753df
Matt Benjamin <matt.g.benjamin@gmail.com>
2015-05-11 20:25:46 +0900

    Reorganize codebase

--- js/app.js
+++ js/app.js
@@ -1,48 +1,37 @@
+/*** TODOs ***
+
+-organize code and comments
+
+-search display
+    when search box is selected (cursor is inside form for typing) display search field selection
+    (i.e. all, artist, tags, venue, street/address, etc.)
+-list view
+    can view by event/artist, venue, tags
+-event info display (make html content)
+    -infoWindow
+    -list view
+    -2 buttons to add:
+        -show on map(in list events) : pan to map marker and open infoWindow
+        -more info(in markers and list view): open info window style pop up(but not actually an info window)
+            -large box, displays lots of additional info, including from other APIs
+
+*/
 // Custom Handler for Google Map
 var map;
-// TODO: implement map markers as computed function based on filteredList, otherwise mapMarkers and created but never deleted
-var googleMapMarkers = []
-ko.bindingHandlers.googlemap = {
-    //map: null,
-    /*
-    createMarkers: function(mapMarkers) {
-        console.log(mapMarkers);
-        var infoWindow = new google.maps.InfoWindow();
-        for (var i = 0; i < mapMarkers.length; i++){
-            //console.log(mapMarkers[i].venue);
-            var latLng = new google.maps.LatLng(
-                            mapMarkers[i].venue.location['geo:point']['geo:lat'],
-                            mapMarkers[i].venue.location['geo:point']['geo:long']);
 
-            // TODO: save these markers to an array / observable array
-            var marker = new google.maps.Marker({
-                position: latLng,
-                title: mapMarkers[i].title,
-                content: mapMarkers[i].title,       // TODO: make function(outside of viewmodel) that sets HTML content
-                map: map
-            });
-            google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.setContent(this.content);
-                infoWindow.open(map, this);
-            });
-            googleMapMarkers.push(marker);
-        }
-    }, */
-    init: function (element, valueAccessor) {
+ko.bindingHandlers.googlemap = {
+    init: function (element) {
         var mapOptions = {
             zoom: 13,
-            //center: { lat: valueAccessor.l}
         };
         map = new google.maps.Map(element, mapOptions);
     },
     update: function (element, valueAccessor) {
+        console.log('update map');
         var value = valueAccessor();
-        console.log(value);
         var latitude = value.mapCenter.latitude;
         var longitude = value.mapCenter.longitude;
-        console.log(latitude);
         map.setCenter( { lat: latitude, lng: longitude } );
-        //ko.bindingHandlers.googlemap.createMarkers(value.lastFmEvents);
     }
 };
 
@@ -52,32 +41,23 @@ var ViewModel =  function () {
     self.currentAddress = ko.observable(self.defaultLocation);
     self.mapCenter = ko.observable( { latitude: 30.267153, longitude: -97.74306079999997 } );
     self.lastFmEvents = ko.observableArray();
-    // TODO: search function
-    //      can search through all aspects of an event (artists, venue, keywords, genre, etc)
-    //      or can select a partcular aspect to search for
-    //      when search box is selected (cursor is inside form for typing) display search field selection
-    self.selectMarker = function(event) {
-        // TODO: 2 buttons
-        //  show on map --> pan to marker and infoWindow (trigger click event)
-        //  more info --> pop up a new box (on top of everything with z-index) with lots of info
-        //              about artists, venue, tickets, etc
-        //  can close (or toggle by clicking list item again), or select a new list item
+    self.filteredList = ko.observableArray([]);
+    self.searchInput = ko.observable();
 
-        // TODO: rewrite this? uses title to find marker :( ...use forEach instead?
+    self.selectMarker = function(event) {
+        // TODO: rewrite this? uses title to find marker :( ...also use forEach instead (check rest of code
+        // for other forReach possibilities)?
         for (var i = 0; i < self.mapMarkers().length; i++) {
             if (event.title === self.mapMarkers()[i].title){
-                //console.log('event name :', event.title);
-                //console.log('marker name :', googleMapMarkers[i].title);
                 google.maps.event.trigger(self.mapMarkers()[i], 'click');
             }
         }
-    }
+    };
 
     var geocoder = new google.maps.Geocoder();
     self.getMapGeocode = ko.computed(function() {
         geocoder.geocode( { 'address': self.currentAddress() }, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
-                    //console.log(results);
                     var latitude = results[0]['geometry']['location']['A'];
                     var longitude = results[0]['geometry']['location']['F'];
                     var mapCenter = {
@@ -85,8 +65,6 @@ var ViewModel =  function () {
                         longitude: longitude
                     };
                     if (latitude != self.mapCenter().latitude && longitude != self.mapCenter().longitude) {
-                        //console.log('Old map latlng: ', self.mapCenter());
-                        //console.log('New map latlng: ', mapCenter);
                         self.mapCenter(mapCenter);
                     } else {
                         console.log('init');
@@ -104,7 +82,7 @@ var ViewModel =  function () {
             var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&' +
                 'lat=' + latitude + '&' +
                 'long=' + longitude + '&' +
-                'limit=10&' +               // TODO: fine tune OR make editable or self correcting
+                'limit=20&' +               // TODO: fine tune OR make editable or self correcting
                 'api_key=d824cbbb7759624aa8b3621a627b70b8' +
                 '&format=json'
             var requestSettings = {
@@ -118,10 +96,7 @@ var ViewModel =  function () {
             $.ajax(requestURL,requestSettings)
         }
     });
-    // TODO: observable based on search bar
-    // observable calls self.searchLastFmEvents and puts results into a filtered array (or filtered array is like current cat...)
 
-    self.filteredList = ko.observableArray([]);
 
     self.mapMarkers = ko.computed(function() {
         var markers = []
@@ -140,10 +115,7 @@ var ViewModel =  function () {
                 icon: 'images/red.png',
                 map: map
             });
-            /*
-            var infoWindow = new google.maps.InfoWindow({
-                //content: marker.content
-            }); */
+
             google.maps.event.addListener(marker, 'click', function() {
                 infoWindow.setContent(this.content);
                 infoWindow.open(map, this);
@@ -155,26 +127,21 @@ var ViewModel =  function () {
     });
 
     self.mapMarkersSearch = ko.computed(function() {
-        var events = self.lastFmEvents()
+        var events = self.lastFmEvents();
         for (var i = 0; i < events.length; i++) {
             var searchedFor = self.filteredList().indexOf(events[i]) > -1;
             if (self.filteredList() == self.lastFmEvents()) {
                 self.mapMarkers()[i].setIcon('images/red.png');
             } else if (searchedFor) {
-                // TODO: custom color
                 self.mapMarkers()[i].setIcon('images/blue.png');
             } else {
-                // TODO: no color
                 self.mapMarkers()[i].setIcon('images/clear.png');
             }
         }
     });
-    self.searchInput = ko.observable();
 
     self.doesStringContain = function (targetString, searchTerm) {
         targetString = targetString.toLowerCase();
-        //console.log(targetString);
-        //console.log(targetString.indexOf(searchTerm) > -1);
         return targetString.indexOf(searchTerm) > -1;
     };
     self.doesListContain = function(targetList, searchTerm) {
