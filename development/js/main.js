5c6e3fa475729eb6a1162c77b79c0913107f4080
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-09-11 16:35:01 -0500

    chore: enable ko component registration explicitly through requirejs

--- development/js/main.js
+++ development/js/main.js
@@ -1,7 +1,7 @@
 // main.js - launch app w/ require dependencies
 require(['init'], function () {
     require(['async!https://maps.googleapis.com/maps/api/js?key=AIzaSyA8B9NC0lW-vqhQzWmVp8XwEMFbyg01blI&libraries=places'], function () {
-        require(['knockout', 'app', '../kocomponents/register-kocomponents'],
+        require(['knockout', 'app', 'registerKoComponents'],
             function (ko, ViewModel) {
             ko.applyBindings(ViewModel);
         });
